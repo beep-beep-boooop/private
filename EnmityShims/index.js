5e9e3720e00a67c358ab5beb08e85b46586d36a4
hmm-monke <hmm-monke@users.noreply.github.com>
2023-12-08 16:01:55 +0000

    deploy: hmm-monke/vendetta-plugins-source@cbf6b1f48549515ea5b2f6b76726dcab6740fc2c

--- EnmityShims/index.js
+++ EnmityShims/index.js
@@ -1,1 +1,1 @@
-(function(){"use strict"})();
+(function(e){"use strict";let n=[];var t={onLoad:function(){n.push(function(){window.enmity={api:{assets:{},clyde:{},commands:{},native:{},settings:{}},common:{},components:{},managers:{plugins:{},themes:{}},metro:{},patcher:{},utilities:{createStore:{},debounce:{},findInReactTree:{},findInTree:{},memoize:{},sleep:{},uuid:{},wrapInHooks:{}}}})},onUnload:function(){for(const o of n)o()}};return e.default=t,Object.defineProperty(e,"__esModule",{value:!0}),e})({});
